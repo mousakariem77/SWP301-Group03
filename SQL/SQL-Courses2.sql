0c1e0fd9630c9537151ffa2d2dbd4fdccf5a766b
Ka Riem <mousakariem27@gmail.com>
2024-01-22 02:26:27 +0700

    post

---
+++ SQL/SQL-Courses2.sql
@@ -0,0 +1,322 @@
+﻿CREATE DATABASE Course_Web
+GO
+
+USE Course_Web
+GO
+
+CREATE TABLE [admin]
+(
+    [adminID] VARCHAR(10) PRIMARY KEY,
+	[name] NVARCHAR(50),
+    [username] VARCHAR(50) NOT NULL,
+    [password] VARCHAR(50) NOT NULL
+);
+GO
+
+CREATE TABLE [learner]
+(
+    [learnerID] VARCHAR(10) PRIMARY KEY,
+	[wallet] DECIMAL(10, 2),
+    [status] INT DEFAULT 0
+);
+GO
+
+CREATE TABLE [instructor]
+(
+    [instructorID] VARCHAR(10) PRIMARY KEY,
+	[income] DECIMAL(10, 2),
+    [status] INT DEFAULT 0
+);
+GO
+
+CREATE TABLE [user]
+(
+    [userID] VARCHAR(10) PRIMARY KEY,
+    [first_name] NVARCHAR(50),
+    [last_name] NVARCHAR(50),
+	[gender] NVARCHAR(10),
+    [birthday] DATE,
+	[phoneNumber] VARCHAR(20),
+    [email] VARCHAR(320),
+    [email_verified] BIT DEFAULT 0 NOT NULL,
+	[country] NVARCHAR(255),
+	[role] NVARCHAR(255),
+    [username] VARCHAR(50)   NOT NULL,
+    [password] VARCHAR(50)   NOT NULL,
+    [picture] TEXT,
+	[registration_Date] DATE,
+	FOREIGN KEY (userID) REFERENCES [learner](learnerID),
+	FOREIGN KEY (userID) REFERENCES [instructor](instructorID)
+);
+GO
+
+CREATE TABLE [categories] (
+    [categoryID] VARCHAR(10) PRIMARY KEY,
+    [category_name] NVARCHAR(255),
+    [description] NTEXT
+);
+GO
+
+CREATE TABLE [courses] (
+    [courseID] VARCHAR(10) PRIMARY KEY,
+    [course_name] NVARCHAR(255),
+    [description] NTEXT,
+    [categoryID] VARCHAR(10),
+	[picture] TEXT,
+	[total_time] INT DEFAULT 0,
+    [price] DECIMAL(10, 2),
+    [startDate] DATE,
+    [endDate] DATE,
+	[rating] INT CHECK (rating >= 1 AND rating <= 5),
+	[number_of_rated] INT DEFAULT 0,
+    [status] NVARCHAR(20),
+    FOREIGN KEY (categoryID) REFERENCES [categories](categoryID)
+);
+GO
+
+CREATE TABLE [instruct]
+(
+    [instructID] VARCHAR(10) PRIMARY KEY,
+    [courseID] VARCHAR(10),
+    [instructorID] VARCHAR(10),
+    FOREIGN KEY (instructorID) REFERENCES [instructor](instructorID),
+    FOREIGN KEY (courseID) REFERENCES [courses](courseID),
+    UNIQUE (instructorID, courseID)
+);
+GO
+
+CREATE TABLE [chapter]
+(
+    [chapterID] VARCHAR(10) PRIMARY KEY,
+    [courseID] VARCHAR(10),
+    [chapter_name] NVARCHAR(50),
+    [index] INT,
+    [description] NTEXT,
+    [total_time] INT DEFAULT 0,
+    FOREIGN KEY (courseID) REFERENCES [courses](courseID)
+);
+GO
+
+CREATE TABLE [lesson]
+(
+    [lessonID] VARCHAR(10) PRIMARY KEY,
+    [chapterID] VARCHAR(10),
+    [lesson_name] NVARCHAR(50),
+    [description] NTEXT,
+    [percent_to_passed] INT DEFAULT 80,
+    [must_be_completed] BIT DEFAULT 1,
+    [content] NTEXT,
+    [index] INT,
+    [type] INT,
+    [time] INT DEFAULT 0 ,
+    FOREIGN KEY (chapterID) REFERENCES [chapter](chapterID)
+);
+GO
+
+CREATE TABLE [course_progress]
+(
+    [course_progressID] INT IDENTITY (1,1)PRIMARY KEY,
+    [learnerID] VARCHAR(10),
+    [courseID] VARCHAR(10),
+    [enrolled] BIT DEFAULT 0,
+    [completed] BIT DEFAULT 0,
+    [progress_percent] INT DEFAULT 0,
+    [rated] BIT DEFAULT 0,
+    [rating] INT CHECK (rating >= 1 AND rating <= 5),
+    [total_time] INT DEFAULT 0,
+    [start_at] DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (learnerID) REFERENCES [learner](learnerID),
+    FOREIGN KEY (courseID) REFERENCES [courses](courseID)
+);
+GO
+
+CREATE TABLE [chapter_progress]
+(
+    [chapter_progressID] INT IDENTITY (1,1) PRIMARY KEY,
+    [chapterID] VARCHAR(10),
+    [course_progressID] INT,
+    [progress_percent] INT DEFAULT 0,
+    [completed] BIT DEFAULT 0,
+    [total_time] INT DEFAULT 0,
+    [start_at] DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (chapterID) REFERENCES [chapter](chapterID),
+    FOREIGN KEY (course_progressID) REFERENCES [course_progress](course_progressID)
+);
+GO
+
+CREATE TABLE [lesson_progress]
+(
+    [lesson_progressID] INT IDENTITY (1,1) PRIMARY KEY,
+    [lessonID] VARCHAR(10),
+    [chapter_progressID] INT,
+    [progress_percent] INT DEFAULT 0,
+    [completed] BIT DEFAULT 0,
+    [start_at] DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (lessonID) REFERENCES [lesson](lessonID),
+    FOREIGN KEY (chapter_progressID) REFERENCES [chapter_progress](chapter_progressID)
+);
+GO
+
+CREATE TABLE [question]
+(
+    [questionID] INT IDENTITY (1,1) PRIMARY KEY,
+    [lessonID] VARCHAR(10),
+	[startTime] DATETIME DEFAULT CURRENT_TIMESTAMP,
+    [endTime] DATETIME DEFAULT CURRENT_TIMESTAMP,
+    [index] INT,
+    [content] NTEXT,
+    [type] INT,
+    [mark] INT DEFAULT 1,
+    FOREIGN KEY (lessonID) REFERENCES [lesson](lessonID)
+);
+GO
+
+CREATE TABLE [answer]
+(
+    [answerID] INT IDENTITY (1,1) PRIMARY KEY,
+    [questionID] INT,
+    [content] NTEXT,
+    [correct] BIT DEFAULT 0,
+    --True: 1, False: 0
+    FOREIGN KEY (questionID) REFERENCES [question](questionID)
+);
+GO
+
+CREATE TABLE [question_result]
+(
+    [question_resultID] INT IDENTITY (1,1) PRIMARY KEY,
+    [lessonID] VARCHAR(10),
+    [lesson_progressID] INT,
+    [number_of_correct_answer] INT,
+    [number_of_question] INT,
+    [mark] INT,
+    [finished] BIT DEFAULT 0,
+    [start_at]  DATETIME,
+    [end_at]  DATETIME,
+    FOREIGN KEY (lessonID) REFERENCES [lesson](lessonID),
+    FOREIGN KEY (lesson_progressID) REFERENCES [lesson_progress](lesson_progressID)
+);
+GO
+
+CREATE TABLE [chosen_answer]
+(
+    [chosen_answerID] INT IDENTITY (1,1)PRIMARY KEY,
+    [questionID] INT,
+    [answerID] INT,
+    [question_resultID] INT,
+    [correct] BIT DEFAULT 0,
+    FOREIGN KEY (question_resultID) REFERENCES [question_result](question_resultID),
+    FOREIGN KEY (answerID) REFERENCES [answer](answerID),
+    FOREIGN KEY (questionID) REFERENCES [question](questionID)
+);
+GO
+
+CREATE TABLE [transaction]
+(
+    [transactionID] INT IDENTITY (1,1)PRIMARY KEY,
+    [learnerID] VARCHAR(10),
+    [courseID] VARCHAR(10),
+	[enrollment_date] DATE,
+    [origin_price]  NUMERIC(10, 2),
+    [price] NUMERIC(10, 2),
+    --[type] INT DEFAULT 0,
+    [description] NTEXT,
+    [status] INT DEFAULT 0,
+    FOREIGN KEY (learnerID) REFERENCES [learner](learnerID),
+    FOREIGN KEY (courseID) REFERENCES [courses](courseID)
+);
+GO
+
+CREATE TABLE [notification] (
+    [notificationID] INT PRIMARY KEY,
+    [receiverID] VARCHAR(10),
+	[receiverType] NVARCHAR(50), --- Loại người nhận (learner, instructor, admin, etc.)
+    [messageType] NVARCHAR(50), -- Loại thông báo (ví dụ: Announcement, QuizReminder, etc.)
+    [message] NTEXT,
+    [sent_date] DATETIME,
+    [isRead] BIT, -- Kiểm tra xem thông báo đã được đọc chưa
+	FOREIGN KEY (ReceiverID) REFERENCES [admin](adminID) ON DELETE CASCADE,
+    FOREIGN KEY (ReceiverID) REFERENCES [learner](learnerID) ON DELETE CASCADE,
+    FOREIGN KEY (ReceiverID) REFERENCES [instructor](instructorID) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [discountCodes] (
+    [discountCodeID] VARCHAR(10) PRIMARY KEY,
+    [instructorID] VARCHAR(10),
+    [courseID] VARCHAR(10),
+    [code] NVARCHAR(20) UNIQUE,
+    [percent_discount] INT, --tỉ lệ phần trăm giảm giá
+	[start_at] DATETIME,
+    [end_at]   DATETIME,
+    [IsActive] BIT,
+    FOREIGN KEY (instructorID) REFERENCES [instructor](instructorID),
+    FOREIGN KEY (courseID) REFERENCES [courses](courseID)
+);
+
+CREATE TABLE [review] (
+    [reviewID] INT PRIMARY KEY,
+    [learnerID] VARCHAR(10),
+    [instructorID] VARCHAR(10),
+	[courseID] VARCHAR(10),
+    [rating] INT CHECK (Rating >= 1 AND Rating <= 5),
+    [comment] NTEXT,
+    [img] TEXT,
+    [icon] VARCHAR(50),
+    [review_date] DATE,
+    FOREIGN KEY (learnerID) REFERENCES [learner](learnerID),
+    FOREIGN KEY (instructorID) REFERENCES [instructor](instructorID),
+	FOREIGN KEY (courseID) REFERENCES [courses](courseID)
+);
+GO
+
+--------------------------------------các bảng nâng cao, mở rộng nếu kịp---------------------------------------
+CREATE TABLE [payment] (
+    [paymentID] VARCHAR(10) PRIMARY KEY,
+    [learnerID] VARCHAR(10),
+	[account_number] INT,
+	[account_name] NVARCHAR(255),
+	[bank] NVARCHAR(255),
+    [status] BIT DEFAULT 0,
+    FOREIGN KEY (learnerID) REFERENCES learner(learnerID),
+);
+GO
+
+CREATE TABLE [AdvertisingPackage]
+(
+    [packageID] INT IDENTITY (1,1) PRIMARY KEY,
+    [instructorID] VARCHAR(10),
+    [startDate] DATE,
+    [endDate] DATE,
+    [price] NUMERIC(10, 2),
+    [status] INT DEFAULT 0,
+    FOREIGN KEY (instructorID) REFERENCES instructor(instructorID)
+);
+
+CREATE TABLE [Receiver_Messages] (
+    [MessageID] INT PRIMARY KEY,
+	[UserID] VARCHAR(10),
+    [MessageType] NVARCHAR(50), -- Loại tin nhắn (ví dụ: TextMessage, ImageMessage, etc.)
+    [MessageText] NTEXT,
+    [SentDate] DATETIME,
+    [IsRead] BIT, -- Kiểm tra xem tin nhắn đã được đọc chưa
+    FOREIGN KEY (UserID) REFERENCES learner(learnerID) ON DELETE CASCADE,
+    FOREIGN KEY (UserID) REFERENCES instructor(instructorID) ON DELETE CASCADE
+	-- tự động xóa các tin nhắn liên quan khi một sinh viên hoặc giáo viên bị xóa khỏi hệ thống
+);
+GO
+
+CREATE TABLE [Send_Messages] 
+(
+    [MessageID] INT PRIMARY KEY,
+	[UserID] VARCHAR(10),
+    [IsSender] BIT,
+    [MessageType] NVARCHAR(50), -- Loại tin nhắn (ví dụ: TextMessage, ImageMessage, etc.)
+    [MessageText] NTEXT,
+    [SentDate] DATETIME,
+    [IsRead] BIT, -- Kiểm tra xem tin nhắn đã được đọc chưa
+    FOREIGN KEY (UserID) REFERENCES learner(learnerID) ON DELETE CASCADE,
+    FOREIGN KEY (UserID) REFERENCES instructor(instructorID) ON DELETE CASCADEE
+	-- tự động xóa các tin nhắn liên quan khi một sinh viên hoặc giáo viên bị xóa khỏi hệ thống
+);
+GO
---
+++ assets/js/main.js
@@ -0,0 +1,246 @@
+/**
+ * Main
+ */
+function time() {
+  var today = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+  var day = weekday[today.getDay()];
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  nowTime = h + "h " + m + "m " + s + "s";
+  if (dd < 10) {
+    dd = '0' + dd
+  }
+  if (mm < 10) {
+    mm = '0' + mm
+  }
+  today = day + ', ' + dd + '/' + mm + '/' + yyyy;
+  tmp = '<span class="date"> ' + today + ' - ' + nowTime +
+    '</span>';
+  document.getElementById("clock").innerHTML = tmp;
+  clocktime = setTimeout("time()", "1000", "Javascript");
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+}
+
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+  toggleLocalStorage();
+  toggleRootClass();
+});
+
+function toggleRootClass(){
+  const current = document.documentElement.getAttribute('data-bs-theme');
+  const inverted = current == 'dark' ? 'light' : 'dark';
+  document.documentElement.setAttribute('data-bs-theme', inverted);
+};
+
+function toggleLocalStorage(){
+  if(isLight()){
+    localStorage.removeItem("light");
+  }else{
+    localStorage.setItem("light", "set");
+  }
+};
+
+function isLight(){
+  return localStorage.getItem("light");
+};
+
+if(isLight()){
+  toggleRootClass();
+}
+
+'use strict';
+
+let menu, animate;
+
+(function () {
+  // Initialize menu
+  //-----------------
+
+  let layoutMenuEl = document.querySelectorAll('#layout-menu');
+  layoutMenuEl.forEach(function (element) {
+    menu = new Menu(element, {
+      orientation: 'vertical',
+      closeChildren: false
+    });
+    // Change parameter to true if you want scroll animation
+    window.Helpers.scrollToActive((animate = false));
+    window.Helpers.mainMenu = menu;
+  });
+
+  // Initialize menu togglers and bind click on each
+  let menuToggler = document.querySelectorAll('.layout-menu-toggle');
+  menuToggler.forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      window.Helpers.toggleCollapsed();
+    });
+  });
+
+  // Display menu toggle (layout-menu-toggle) on hover with delay
+  let delay = function (elem, callback) {
+    let timeout = null;
+    elem.onmouseenter = function () {
+      // Set timeout to be a timer which will invoke callback after 300ms (not for small screen)
+      if (!Helpers.isSmallScreen()) {
+        timeout = setTimeout(callback, 300);
+      } else {
+        timeout = setTimeout(callback, 0);
+      }
+    };
+
+    elem.onmouseleave = function () {
+      // Clear any timers set to timeout
+      document.querySelector('.layout-menu-toggle').classList.remove('d-block');
+      clearTimeout(timeout);
+    };
+  };
+  if (document.getElementById('layout-menu')) {
+    delay(document.getElementById('layout-menu'), function () {
+      // not for small screen
+      if (!Helpers.isSmallScreen()) {
+        document.querySelector('.layout-menu-toggle').classList.add('d-block');
+      }
+    });
+  }
+
+  // Display in main menu when menu scrolls
+  let menuInnerContainer = document.getElementsByClassName('menu-inner'),
+    menuInnerShadow = document.getElementsByClassName('menu-inner-shadow')[0];
+  if (menuInnerContainer.length > 0 && menuInnerShadow) {
+    menuInnerContainer[0].addEventListener('ps-scroll-y', function () {
+      if (this.querySelector('.ps__thumb-y').offsetTop) {
+        menuInnerShadow.style.display = 'block';
+      } else {
+        menuInnerShadow.style.display = 'none';
+      }
+    });
+  }
+
+  // Init helpers & misc
+  // --------------------
+
+  // Init BS Tooltip
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  // Accordion active class
+  const accordionActiveFunction = function (e) {
+    if (e.type == 'show.bs.collapse' || e.type == 'show.bs.collapse') {
+      e.target.closest('.accordion-item').classList.add('active');
+    } else {
+      e.target.closest('.accordion-item').classList.remove('active');
+    }
+  };
+
+  const accordionTriggerList = [].slice.call(document.querySelectorAll('.accordion'));
+  const accordionList = accordionTriggerList.map(function (accordionTriggerEl) {
+    accordionTriggerEl.addEventListener('show.bs.collapse', accordionActiveFunction);
+    accordionTriggerEl.addEventListener('hide.bs.collapse', accordionActiveFunction);
+  });
+
+  // Auto update layout based on screen size
+  window.Helpers.setAutoUpdate(true);
+
+  // Toggle Password Visibility
+  window.Helpers.initPasswordToggle();
+
+  // Speech To Text
+  window.Helpers.initSpeechToText();
+
+  // Manage menu expanded/collapsed with templateCustomizer & local storage
+  //------------------------------------------------------------------
+
+  // If current layout is horizontal OR current window screen is small (overlay menu) than return from here
+  if (window.Helpers.isSmallScreen()) {
+    return;
+  }
+
+  // If current layout is vertical and current window screen is > small
+
+  // Auto update menu collapsed/expanded based on the themeConfig
+  window.Helpers.setCollapsed(true, false);
+})();
+
+(function ($) {
+  "use strict";
+  
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+      function toggleNavbarMethod() {
+          if ($(window).width() > 992) {
+              $('.navbar .dropdown').on('mouseover', function () {
+                  $('.dropdown-toggle', this).trigger('click');
+              }).on('mouseout', function () {
+                  $('.dropdown-toggle', this).trigger('click').blur();
+              });
+          } else {
+              $('.navbar .dropdown').off('mouseover').off('mouseout');
+          }
+      }
+      toggleNavbarMethod();
+      $(window).resize(toggleNavbarMethod);
+  }); 
+  
+  
+  // Back to top button
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+  });
+  $('.back-to-top').click(function () {
+      $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+      return false;
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1500,
+      dots: true,
+      loop: true,
+      items: 1
+  });
+  
+})(jQuery);
+
+
+const passwordInput = document.getElementById('passwordInput');
+const togglePassword = document.querySelector('.toggle-password');
+
+togglePassword.addEventListener('click', function() {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    togglePassword.classList.remove('fa-eye');
+    togglePassword.classList.add('fa-eye-slash');
+  } else {
+    passwordInput.type = 'password';
+    togglePassword.classList.remove('fa-eye-slash');
+    togglePassword.classList.add('fa-eye');
+  }
+});
